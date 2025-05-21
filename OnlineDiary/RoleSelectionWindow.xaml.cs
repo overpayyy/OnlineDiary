03dd6321995c7f9e6f6b76a4bf03a3b667053a92
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-21 15:24:43 +0200

    role selection window logic

--- OnlineDiary/RoleSelectionWindow.xaml.cs
+++ OnlineDiary/RoleSelectionWindow.xaml.cs
@@ -1,28 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+﻿using System.Windows;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OnlineDiary
 {
-    /// <summary>
-    /// Interaction logic for RoleSelectionWindow.xaml
-    /// </summary>
     public partial class RoleSelectionWindow : Window
     {
         public RoleSelectionWindow()
         {
             InitializeComponent();
+        }
+
+        private void StudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            var studentLogin = new LoginWindowStudent();
+            NavigationService.GetNavigationService(this)?.Navigate(studentLogin);
+            this.Close();
+        }
+
+        private void TeacherButton_Click(object sender, RoutedEventArgs e)
+        {
+            var teacherLogin = new LoginWindowTeacher();
+            NavigationService.GetNavigationService(this)?.Navigate(teacherLogin);
+            this.Close();
         }
     }
 }
