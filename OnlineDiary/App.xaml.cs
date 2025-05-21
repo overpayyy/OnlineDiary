b450d3aeccd468e6e49c958b4c479950f9942063
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-21 13:32:42 +0200

    Add database connection

--- OnlineDiary/App.xaml.cs
+++ OnlineDiary/App.xaml.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using OnlineDiary.Data;
+
+namespace OnlineDiary
+{
+    public partial class App : Application
+    {
+        public static AppDbContext DbContext { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite("Data Source=diary.db")
+                .Options;
+
+            DbContext = new AppDbContext(options);
+            DbContext.Database.Migrate();
+
+            base.OnStartup(e);
+        }
+    }
+}
