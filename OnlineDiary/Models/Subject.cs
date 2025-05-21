2b580fa2637e28610948af68e95984ba347b532f
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-21 13:16:22 +0200

    create a class subject

--- OnlineDiary/Models/Subject.cs
+++ OnlineDiary/Models/Subject.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnlineDiary.Models
+﻿public class Subject
 {
-    internal class Subject
-    {
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public List<Lesson> Lessons { get; set; }
 }
