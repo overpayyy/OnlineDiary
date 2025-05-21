f6711385c8adb69fc65b1b17dae47a3098ece0d9
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-21 13:02:42 +0200

    upgrade user class and create a lesson class

--- OnlineDiary/Models/Lesson.cs
+++ OnlineDiary/Models/Lesson.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OnlineDiary.Models;
 
-namespace OnlineDiary.Models
+public class Lesson
 {
-    internal class Lesson
-    {
-    }
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Time { get; set; }
+    public string Description { get; set; }
+
+    public int SubjectId { get; set; }
+    public Subject Subject { get; set; }
+
+    public int TeacherId { get; set; }
+    public User Teacher { get; set; }
+
+    public List<Grade> Grades { get; set; }
 }
