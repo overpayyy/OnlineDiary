3963ec34ae6182bba34f524ba21b5277962de01a
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-05-21 13:11:59 +0200

    create a class grade

--- OnlineDiary/Models/Grade.cs
+++ OnlineDiary/Models/Grade.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OnlineDiary.Models
+﻿public class Grade
 {
-    internal class Grade
-    {
-    }
+    public int Id { get; set; }
+    public string Value { get; set; }
+
+    public int LessonId { get; set; }
+    public Lesson Lesson { get; set; }
+
+    public int StudentId { get; set; }
+    public User Student { get; set; }
 }
